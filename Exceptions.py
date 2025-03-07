2e8252c528ae957ae11d1cf5a85bd5d3b6f42bd4
Nagalla Shyam Kumar <162819823+shyamkumar31@users.noreply.github.com>
2025-03-07 11:32:56 +0530

    Update Exceptions.py

--- Exceptions.py
+++ Exceptions.py
@@ -1,29 +1,30 @@
-a = [1, 2, 3]
+arr = [1, 2, 3]
 try:
-    print ("Second element = ",a[1])
- 
-    # Throws error since there are only 3 elements in array
-    print ("Fourth element = ",a[3])
-    
+    print("Second element =", arr[1])
+
+    # Throws error since there are only 3 elements in the list
+    print("Fourth element =", arr[3])
+
 except:
-    print ("An error occurred")
+    print("An error occurred")
 
 print()
 
-b = [3,2,1]
+list1 = [3, 2, 1]
+list2 = [1, 2, 3]
 try:
-    a == b
+    list1 == list2
 except:
     print("They are not equal")
 else:
-    print("Both Equal") 
+    print("Both Equal")
 
 print()
 
 try:
-    k = 5/0
-    print(k)
+    result = 5 / 0
+    print(result)
 except ZeroDivisionError:
     print("Can't divide by zero")
 finally:
-    print('This is always executed')                 
+    print("This is always executed")
