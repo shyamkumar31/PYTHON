c692d35f85fe816c9d1f60f4a278f9ddd8d9456f
Nagalla Shyam Kumar <162819823+shyamkumar31@users.noreply.github.com>
2025-03-07 11:33:53 +0530

    Update files.py

--- files.py
+++ files.py
@@ -1,16 +1,19 @@
-# Write a program to read text file.
+# Reading from a text file
+with open("HW.txt", "r") as hw_file:
+    hw_data = hw_file.read()
+    print(hw_data)
 
-file1 = open("HW.txt","r")
-data = file1.read()
-print(data)
 print()
 
-file2 = open("Blank.txt","w")
-str1 = 'Python'
-file2.write(str1)
+# Writing to a text file
+with open("Blank.txt", "w") as blank_file:
+    text_to_write = 'Python'
+    blank_file.write(text_to_write)
+
 print()
 
-file3 = open("HW.txt","r+")
-print(file3.readline(11))
-print()
+# Reading with read+ mode (reading and writing)
+with open("HW.txt", "r+") as hw_file_rp:
+    print(hw_file_rp.readline(11))
 
+print()
